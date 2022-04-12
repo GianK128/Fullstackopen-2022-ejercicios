833c79e5f55ced094d0c6577f918981e6df5a05c
GianK128 <giankeberlein@gmail.com>
2022-04-12 01:53:35 -0300

    Add solution for exercise 1.1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    parts.map(part => {
+      return (
+        <p key={part.title}>
+          {part.title} {part.exercises}
+        </p>
+      )
+    })
+  )
+}
+
+const Total = ({ total }) => {
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +36,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={[
+          {title: part1, exercises: exercises1}, 
+          {title: part2, exercises: exercises2},
+          {title: part3, exercises: exercises3}
+        ]}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
