3e5d9e4866af1c0ffe6480c8d6be68cd99f1ce0c
GianK128 <giankeberlein@gmail.com>
2022-04-13 00:23:12 -0300

    Add solution for exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
+const Title = ({ text }) => <h1>{text}</h1>
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
+  const incrementGood = () => () => {
+    setGood(prevGood => prevGood + 1)
+  }
+  const incrementNeutral = () => () => {
+    setNeutral(prevNeutral => prevNeutral + 1)
+  }
+  const incrementBad = () => () => {
+    setBad(prevBad => prevBad + 1)
+  }
+
   return (
     <div>
-      
+      <Title text={"Give feedback"} />
+      <div>
+        <button onClick={incrementGood()}>
+          Good
+        </button>
+        <button onClick={incrementNeutral()}>
+          Neutral
+        </button>
+        <button onClick={incrementBad()}>
+          Bad
+        </button>
+      </div>
+      <Title text={"Statistics"} />
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
