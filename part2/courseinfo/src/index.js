2115730928da24e5d22ac991e44184cfb942c63d
GianK128 <giankeberlein@gmail.com>
2022-04-15 01:36:42 -0300

    Add solution for exercise 2.1

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 const Header = ({ course }) => {
   return (
-    <h1>{course}</h1>
+    <h2>{course}</h2>
   )
 }
 
@@ -18,7 +18,7 @@ const Content = ({ parts }) => {
     parts.map(part => {
       return (
         <Part
-          key={part.name}
+          key={part.id}
           title={part.name} 
           exercises={part.exercises}
         />
@@ -41,24 +41,27 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
     </div>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7
       },
       {
+        id: 3,
         name: 'State of a component',
         exercises: 14
       }
