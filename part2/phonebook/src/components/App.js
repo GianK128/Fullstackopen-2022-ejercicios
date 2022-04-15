2668cfde988d641c3491345c29304cb7a844c90b
GianK128 <giankeberlein@gmail.com>
2022-04-15 03:23:00 -0300

    Add solution for exercise 2.8

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-1234567' }
   ]) 
-  const [ newName, setNewName ] = useState('New person...')
+  const [newName, setNewName] = useState('New person...')
+  const [newPhone, setNewPhone] = useState('New phone...')
 
   const handleNameSubmit = () => (event) => {
     event.preventDefault()
+    if (newName === '' || newPhone === '')
+    {
+      alert('Please, complete every field before submitting')
+      return
+    }
+    
     const newPerson = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
 
     if (persons.some(p => p.name === newPerson.name))
@@ -20,10 +28,7 @@ const App = () => {
 
     setPersons(persons.concat(newPerson))
     setNewName('')
-  }
-
-  const handleNameChange = () => (event) => {
-    setNewName(event.target.value)
+    setNewPhone('')
   }
 
   return (
@@ -31,12 +36,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleNameSubmit()}>
         <label>Name:</label>
-        <input value={newName} onChange={handleNameChange()}/>
+        <input 
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <label>Phone:</label>
+        <input 
+          value={newPhone} 
+          onChange={(e) => setNewPhone(e.target.value)}
+        />
         <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.phone}</p>
       ))}
     </div>
   )
