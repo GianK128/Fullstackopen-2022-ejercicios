a199397389969a1a6712d5294544cc9533beaabb
GianK128 <giankeberlein@gmail.com>
2022-04-15 02:15:03 -0300

    Add solution for exercise 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,108 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
 
-const Header = ({ course }) => {
-  return (
-    <h2>{course}</h2>
-  )
-}
-
-const Part = ({ title, exercises }) => {
-  return (
-    <p>{title} {exercises}</p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    parts.map(part => {
-      return (
-        <Part
-          key={part.id}
-          title={part.name} 
-          exercises={part.exercises}
-        />
-      )
-    })
-  )
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((prev, curr) => prev + curr.exercises, 0)
-  return (
-    <p><strong>Total of {sum} exercises</strong></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
-
-  return (
-    <div>
-      <h1>Web development curriculum</h1>
-      {courses.map(course => (
-        <Course key={course.id} course={course}/>
-      ))}
-    </div>
-  )
-}
+const courses = [
+  {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1,
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2,
+      },
+    ],
+  },
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App courses={courses}/>
   </React.StrictMode>
 );
