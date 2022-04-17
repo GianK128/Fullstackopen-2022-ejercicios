a89c13a174c2ebb743cccbbebb5dd2d59de16b42
GianK128 <giankeberlein@gmail.com>
2022-04-17 17:36:08 -0300

    Add solution for exercise 2.11

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Contacts from './Contacts'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' }
-  ])
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const personsToShow = filter === ''
     ? persons
